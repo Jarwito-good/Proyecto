e5bac7de4d40b62756fc88e8f01e81f812afbf79
Jarwito-good <jarwing031@gmail.com>
2026-03-03 00:23:53 -0500

    Update script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+body {
+    font-family: Arial, sans-serif;
+    background-color: #111;
+    color: white;
+    text-align: center;
+    margin-top: 100px;
+}
+
+button {
+    padding: 10px 20px;
+    font-size: 16px;
+    background-color: crimson;
+    color: white;
+    border: none;
+    cursor: pointer;
+    border-radius: 5px;
+}
+
+button:hover {
+    background-color: darkred;
+}
